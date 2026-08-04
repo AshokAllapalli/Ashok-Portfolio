76dab405475c0070ca438070a885f7bffc11c919
AshokAbacco <ashokgowd34@gmail.com>
2026-08-04 23:42:35 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import SplashCursor from "./components/SplashCursor";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
@@ -8,13 +12,14 @@ import Skills from "./pages/Skills";
 function App() {
   return (
     <Router>
+      <SplashCursor />
+
       {/* <Navbar /> */}
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
-
       </Routes>
 
       <Footer />
